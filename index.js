5c0693d99edc4cd094c8531f94bf7676ad6646ed
Ryanwuu <Ryanwuu@outlook.com>
2015-01-07 16:35:20 +0800

    Serving The Root Path

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ module.exports = harp;
 var connect = require ('connect');
 var serveStatic = require('serve-static');
 var makeJade = require ('./lib/processor/jade.js');
-var makeLess =require('./lib/processor/less.js');
-
+var makeLess =  require('./lib/processor/less.js');
+var entry = require ('./lib/processor/entry.js');
 
 function harp(root){
 	return connect()
+				.use(entry)
 				.use(serveStatic(root))
 				.use(makeJade(root))
 				.use(makeLess(root));
